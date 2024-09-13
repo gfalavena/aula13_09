e76bb5884b0291ba35d0b851254151622ce415b2
Gabriel Falavena <gabriel.oliveira1370@etec.sp.gov.br>
2024-09-13 17:14:32 -0300

    primeiro commit

---
+++ src/components/default/ContentPage.vue
@@ -0,0 +1,52 @@
+<template>
+<section>
+    <div>
+        <p>Meu Conteúdo</p>
+        <div>
+            <!-- V-model -->
+            <input v-model="nome" placeholder="Digite seu nome">
+            <p>Seu nome é: {{ nome }}</p>
+
+            <!-- V-click -->
+            <button @click="submeterFormulario()">Procurar usuário</button>
+            
+            <!-- V-if e v-else -->
+            <p v-if="url">
+                {{ url }}
+
+            </p>
+            <p v-else>
+                Você ainda não buscou usuários
+            </p>
+        </div>
+    </div>
+</section>
+</template>
+
+<script>
+
+export default {
+    name: 'ContentPage',
+    data(){
+        return{
+            nome: '',
+            url: ''
+        }
+    },
+    methods: {
+        submeterFormulario(){
+            fetch(`https://api.github.com/users/${this.nome}`)
+            .then((data) => {
+                this.data= data.url
+                console.log(data.url);
+            });
+
+            
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/default/NavPage.vue
@@ -0,0 +1,19 @@
+<template>
+<header>
+    <nav>
+        <ul>
+            <li>Nome</li>
+            <li>Sobre</li>
+            <li>Contato</li>
+        </ul>
+    </nav>
+</header>
+</template>
+
+<script>
+export default {
+    name:"NavPage"
+    }
+</script>
+
+<style></style>
